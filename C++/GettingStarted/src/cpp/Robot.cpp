2844d96d8a7129d9595977883e0a044dffc9ed0d
Will Shamblin <will@shamblin.org>
2017-12-25 11:57:34 -0500

    Added support for all 6 Talons.

--- C++/GettingStarted/src/cpp/Robot.cpp
+++ C++/GettingStarted/src/cpp/Robot.cpp
@@ -4,28 +4,39 @@
 
 class Robot: public IterativeRobot
 {
-	CANTalon *frontLeft;
-	CANTalon *frontRight;
-    CANTalon *middleLeft;
-    CANTalon *middleRight;
-	CANTalon *backLeft;
-	CANTalon *backRight;
+	CANTalon *leftMaster;
+	CANTalon *rightMaster;
+    CANTalon *leftFrontSlave;
+    CANTalon *rightFrontSlave;
+	CANTalon *leftBackSlave;
+	CANTalon *rightBackSlave;
+    //CANTalon *middleLeft;
+    //CANTalon *middleRight;
 
 	Joystick *joystick;
 	RobotDrive *drive;
 public:
 	Robot() {
 
-		frontRight = new CANTalon(4);
-		middleRight = new CANTalon(5);
-		backRight = new CANTalon(6);
+		rightMaster = new CANTalon(4); //Master
+		rightFrontSlave = new CANTalon(5); //Slave
+        rightFrontSlave->SetTalonControlMode(CANTalon::TalonControlMode::kFollowerMode);
+        rightFrontSlave->Set(4);
+        rightBackSlave = new CANTalon(6);  //Slave
+        rightBackSlave->SetTalonControlMode(CANTalon::TalonControlMode::kFollowerMode);
+        rightBackSlave->Set(4);
 
-		frontLeft = new CANTalon(1);
-		middleLeft = new CANTalon(2);
-		backLeft = new CANTalon(3);
+
+        leftMaster = new CANTalon(1); //Master
+		leftFrontSlave = new CANTalon(2); //Slave
+        leftFrontSlave->SetTalonControlMode(CANTalon::TalonControlMode::kFollowerMode);
+        leftFrontSlave->Set(1);
+        leftBackSlave = new CANTalon(3); //Slave
+        leftBackSlave->SetTalonControlMode(CANTalon::TalonControlMode::kFollowerMode);
+        leftBackSlave->Set(1);
 
 		joystick = new Joystick(1);
-		drive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
+		drive = new RobotDrive(leftMaster, rightMaster);
 	}
 
 	void AutonomousPeriodic()
